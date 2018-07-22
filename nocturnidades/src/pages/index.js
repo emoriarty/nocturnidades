80d6e4bf769ec375ccb392b744586e391fa2a931
Enrique Arias <emoriarty81@gmail.com>
2018-07-22 10:42:36 +0200

    Localised dates

--- nocturnidades/src/pages/index.js
+++ nocturnidades/src/pages/index.js
@@ -39,7 +39,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "DD MMMM, YYYY")
+            date(formatString: "DD MMMM YYYY", locale: "es")
             title
           }
         }
